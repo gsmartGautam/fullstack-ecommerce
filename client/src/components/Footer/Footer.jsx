25c3b4a9280254d050c3ad8e75efcf397f29530d
Gautam Rana <gsmartgautam@gmail.com>
2023-05-03 16:07:53 +0530

    initial commit

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
+
+const Footer = () => {
+    return <footer className="footer">
+        <div className="footer-content">
+            <div className="col">
+                <div className="title">About</div>
+                <div className="text">
+                    Voluptatem accusantium doloremque laudantium, totam rem
+                    aperiam, eaque ipsa quae ab illo inventore veritatis et
+                    quasi architecto beatae vitae dicta sunt explicabo eaque
+                    ipsa quae ab illo.
+                </div>
+            </div>
+            <div className="col">
+                <div className="title">Contact</div>
+                <div className="c-item">
+                    <FaLocationArrow />
+                    <div className="text">
+                        Kalyani Rd, Punnamda, Kottankulangara, Alappuzha,
+                        Kerla, 688006
+                    </div>
+                </div>
+                <div className="c-item">
+                    <FaMobileAlt />
+                    <div className="text">Phone: 0471 272 0261</div>
+                </div>
+                <div className="c-item">
+                    <FaEnvelope />
+                    <div className="text">Email:- store@dev.com</div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="title">Categories</div>
+                <span className="text">Headphone</span>
+                <span className="text">Smart Watches</span>
+                <span className="text">Blutooth Speaker</span>
+                <span className="text">Wireless Earbuds</span>
+                <span className="text">Home Theater</span>
+                <span className="text">Projectors</span>
+            </div>
+            <div className="col">
+            <div className="title">Pages</div>
+                <span className="text">Home</span>
+                <span className="text">About</span>
+                <span className="text">Privecy Policy</span>
+                <span className="text">Returns</span>
+                <span className="text">Terms & Conditions</span>
+                <span className="text">Contact Us</span>
+            </div>
+        </div>
+        <div className="bottom-bar">
+            <div className="bottom-bar-content">
+                <div className="text">
+                    JSDEVSTORE 2022 CREATED BY GAUTAM RANA - Premium E-Commerce Solutions
+                </div>
+                <img src={Payment} />
+            </div>
+        </div>
+    </footer>;
+};
+
+export default Footer;
---
+++ client/src/components/Home/Category/Category.jsx
@@ -0,0 +1,55 @@
+import {useNavigate} from "react-router-dom";
+import "./Category.scss";
+
+const Category = ({categories}) => {
+    const navigate = useNavigate();
+    return (
+        <div className="shop-by-category">
+            <div className="categories">
+                {categories?.data?.map((item) => (
+                    <div 
+                        key={item.id} 
+                        className="category" 
+                        onClick={()=>navigate(`/category/${item.id}`)}
+                    >
+                        <img
+                            src = {
+                                process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url
+                            }
+                            alt=""
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Category;
+
+
+// import { useParams } from "react-router-dom";
+// import useFetch from "../../../hooks/useFetch";
+// import Products from "../../Products/Products";
+// import "./Category.scss";
+// const Category = () => {
+//     const { id } = useParams();
+//     const { data } = useFetch(
+//         `/api/products?populate=*&[filters][categories][id]=${id}`
+//     );
+//     return (
+//         <div className="category-main-content">
+//             <div className="layout">
+//                 <div className="category-title">
+//                     {
+//                         data?.data?.[0]?.attributes?.categories?.data?.[0]
+//                             ?.attributes?.title
+//                     }
+//                 </div>
+//                 <Products innerPage={true} products={data} />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Category;
